160327464bffa80846b3d006b8c4a70b46d744dd
ElKidd7 <ramirez.axel34@uabc.edu.mx>
2024-10-15 21:17:54 -0700

    LAB POO 2.4.1

---
+++ src/lab/pkg2/pkg4/pkg1/Bicicleta.java
@@ -0,0 +1,68 @@
+package lab.pkg2.pkg4.pkg1;
+
+public class Bicicleta {
+    String marca;
+    String modelo;    
+    int numeroMarchas;
+
+    
+    
+    
+       public  Rueda rueda2 = new Rueda();
+        public Cuadro cuadro1= new Cuadro();
+        public Rueda rueda1 = new Rueda();
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getNumeroMarchas() {
+        return numeroMarchas;
+    }
+
+    public void setNumeroMarchas(int numeroMarchas) {
+        this.numeroMarchas = numeroMarchas;
+    }
+
+    public Cuadro getCuadro1() {
+        return cuadro1;
+    }
+
+    public void setCuadro1(Cuadro cuadro1) {
+        this.cuadro1 = cuadro1;
+    }
+
+    public Rueda getRueda1() {
+        return rueda1;
+    }
+
+    public void setRueda1(Rueda rueda1) {
+        this.rueda1 = rueda1;
+    }
+
+    public Rueda getRueda2() {
+        return rueda2;
+    }
+
+    public void setRueda2(Rueda rueda2) {
+        this.rueda2 = rueda2;
+    }
+
+   
+
+   
+    }
+    
+
---
+++ src/lab/pkg2/pkg4/pkg1/Rueda.java
@@ -0,0 +1,36 @@
+
+package lab.pkg2.pkg4.pkg1;
+
+public class Rueda {
+    
+    double diametro;
+    String color;
+    String tipoLlanta;
+
+    public double getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(double diametro) {
+        this.diametro = diametro;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipoLlanta() {
+        return tipoLlanta;
+    }
+
+    public void setTipoLlanta(String tipoLlanta) {
+        this.tipoLlanta = tipoLlanta;
+    }
+    
+    
+    
+}
